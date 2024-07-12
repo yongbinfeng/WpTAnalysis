76b7f71f52ae8e46b296e3edf62a0e49d0de96be
Yongbin Feng <ybfeng94@gmail.com>
2024-07-12 11:27:03 +0200

    update for hepdata

--- compXSec.py
+++ compXSec.py
@@ -28,7 +28,7 @@ for key in xsecs['ppbar']['Wp'].keys():
     
 print("xsecs: ", xsecs)
 
-def ThXSec2TGraph(xsecs, color = 4):
+def ThXSec2TGraph(xsecs, color = 4, name = None, title = None):
     n = len(xsecs)
     sqrtSs = np.array([key/1.0e3 for key in xsecs.keys()], dtype='d')
     values = np.array([xsecs[key] for key in xsecs.keys()], dtype='d')
@@ -37,9 +37,13 @@ def ThXSec2TGraph(xsecs, color = 4):
     g = ROOT.TGraph(n, sqrtSs, values)
     g.SetLineColor(color)
     g.SetLineWidth(4)
+    if name:
+        g.SetName(name)
+    if title:
+        g.SetTitle(title)
     return g
 
-def ExpXsec2TGraph(xsecs, markerstyle = 20, color = 1):
+def ExpXsec2TGraph(xsecs, markerstyle = 20, color = 1, name = None, title = None):
     n = len(xsecs)
     sqrts = xsecs.sqrts
     vals = []
@@ -57,19 +61,23 @@ def ExpXsec2TGraph(xsecs, markerstyle = 20, color = 1):
     g.SetLineColor(color)
     g.SetMarkerStyle(markerstyle)
     g.SetMarkerSize(1.5)
+    if name:
+        g.SetName(name)
+    if title:
+        g.SetTitle(title)
     return g
         
     
 import ROOT
 ROOT.gROOT.SetBatch(True)
 from CMSPLOTS.myFunction import DrawHistos
-gZ_ppbar = ThXSec2TGraph(xsecs['ppbar']['Z'])
-gW_ppbar = ThXSec2TGraph(xsecs['ppbar']['Winc'])
+gZ_ppbar = ThXSec2TGraph(xsecs['ppbar']['Z'], name = "gZ_ppbar")
+gW_ppbar = ThXSec2TGraph(xsecs['ppbar']['Winc'], name = "gW_ppbar")
 
-gZ_pp = ThXSec2TGraph(xsecs['pp']['Z'])
-gWp_pp = ThXSec2TGraph(xsecs['pp']['Wp'])
-gWm_pp = ThXSec2TGraph(xsecs['pp']['Wm'])
-gW_pp = ThXSec2TGraph(xsecs['pp']['Winc'])
+gZ_pp = ThXSec2TGraph(xsecs['pp']['Z'], name = "gZ_pp")
+gWp_pp = ThXSec2TGraph(xsecs['pp']['Wp'], name = "gWp_pp")
+gWm_pp = ThXSec2TGraph(xsecs['pp']['Wm'], name = "gWm_pp")
+gW_pp = ThXSec2TGraph(xsecs['pp']['Winc'], name = "gW_pp")
 
 toDraws = [gZ_ppbar, gW_ppbar, gZ_pp, gWp_pp, gWm_pp, gW_pp]
 drawoptions = ["PL", "PL", "PL", "PL", "PL", "PL"]
@@ -77,16 +85,16 @@ legends = ["" for i in range(len(toDraws))]
 legendoptions = ["L", "L", "L", "L", "L", "L"]
 
 from data.expXsecResults import *
-g_CMS_13TeV = ExpXsec2TGraph(xsecs_CMS_13TeV, color = 2, markerstyle = 43)
-g_CMS_5TeV = ExpXsec2TGraph(xsecs_CMS_5TeV, color = 2, markerstyle = 45)
+g_CMS_13TeV = ExpXsec2TGraph(xsecs_CMS_13TeV, color = 2, markerstyle = 43, name = "g_CMS_13TeV", title = f"CMS, {xsecs_CMS_13TeV.GetLegend()}")
+#g_CMS_5TeV = ExpXsec2TGraph(xsecs_CMS_5TeV, color = 2, markerstyle = 45, name = "g_CMS_5TeV")
 
 toDraws += [g_CMS_13TeV]
 drawoptions += ["PZ"]
 legends += [f"CMS, {xsecs_CMS_13TeV.GetLegend()}"]
 legendoptions += ["P"]
 
-g_CMS_8TeV = ExpXsec2TGraph(xsecs_CMS_8TeV)
-g_CMS_7TeV = ExpXsec2TGraph(xsecs_CMS_7TeV, color = 1, markerstyle = 24)
+g_CMS_8TeV = ExpXsec2TGraph(xsecs_CMS_8TeV, name = "g_CMS_8TeV", title = f"CMS, {xsecs_CMS_8TeV.GetLegend()}")
+g_CMS_7TeV = ExpXsec2TGraph(xsecs_CMS_7TeV, color = 1, markerstyle = 24, name = "g_CMS_7TeV", title = f"CMS, {xsecs_CMS_7TeV.GetLegend()}")
 
 toDraws += [g_CMS_8TeV, g_CMS_7TeV]
 drawoptions += ["PZ", "PZ"]
@@ -94,38 +102,38 @@ legendoptions += ["P", "P"]
 legends += [f"CMS, {xsecs_CMS_8TeV.GetLegend()}", f"CMS, {xsecs_CMS_7TeV.GetLegend()}"]
 
 xsecs_ATLAS_7TeV.sqrts = 7.0-0.2
-g_ATLAS_7TeV = ExpXsec2TGraph(xsecs_ATLAS_7TeV, color = 1, markerstyle = 41)
+g_ATLAS_7TeV = ExpXsec2TGraph(xsecs_ATLAS_7TeV, color = 1, markerstyle = 41, name = "g_ATLAS_7TeV", title = f"ATLAS, {xsecs_ATLAS_7TeV.GetLegend()}")
 
-g_CMS_5TeV = ExpXsec2TGraph(xsecs_CMS_5TeV, color = 2, markerstyle = 45)
+g_CMS_5TeV = ExpXsec2TGraph(xsecs_CMS_5TeV, color = 2, markerstyle = 45, name = "g_CMS_5TeV", title = f"CMS, {xsecs_CMS_5TeV.GetLegend()}")
 toDraws += [g_CMS_5TeV]
 drawoptions += ["PZ"]
 legends += [f"CMS, {xsecs_CMS_5TeV.GetLegend()}"]
 legendoptions += ["P"]
 
-g_CMS_2760GeV_2 = ExpXsec2TGraph(xsecs_CMS_2760GeV_2, color = 1, markerstyle = 47)
+g_CMS_2760GeV_2 = ExpXsec2TGraph(xsecs_CMS_2760GeV_2, color = 1, markerstyle = 47, name = "g_CMS_2760GeV_2", title = f"CMS, {xsecs_CMS_2760GeV_2.GetLegend()}")
 toDraws += [g_CMS_2760GeV_2]
 drawoptions += ["PZ"]
 legends += [f"CMS, {xsecs_CMS_2760GeV_2.GetLegend()}"]
 legendoptions += ["P"]
 
-g_CMS_2760GeV = ExpXsec2TGraph(xsecs_CMS_2760GeV, color = 1, markerstyle = 46)
+g_CMS_2760GeV = ExpXsec2TGraph(xsecs_CMS_2760GeV, color = 1, markerstyle = 46, name = "g_CMS_2760GeV", title = f"CMS, {xsecs_CMS_2760GeV.GetLegend()}")
 toDraws += [g_CMS_2760GeV]
 drawoptions += ["PZ"]
 legends += [f"CMS, {xsecs_CMS_2760GeV.GetLegend()}"]
 legendoptions += ["P"]
 
-g_CDF_Run2 = ExpXsec2TGraph(xsecs_CDF_Run2, color = 1, markerstyle = 21)
-g_D0_Run1 = ExpXsec2TGraph(xsecs_D0_Run1, color = 1, markerstyle = 25)
+g_CDF_Run2 = ExpXsec2TGraph(xsecs_CDF_Run2, color = 1, markerstyle = 21, name = "g_CDF_Run2", title = f"CDF, {xsecs_CDF_Run2.GetLegend()}")
+g_D0_Run1 = ExpXsec2TGraph(xsecs_D0_Run1, color = 1, markerstyle = 25, name = "g_D0_Run1", title = f"D0, {xsecs_D0_Run1.GetLegend()}")
 
 toDraws += [g_CDF_Run2, g_D0_Run1]
 drawoptions += ["PZ", "PZ"]
 legendoptions += ["P", "P"]
 legends += [f"CDF, {xsecs_CDF_Run2.GetLegend()}", f"D0, {xsecs_D0_Run1.GetLegend()}"]
 
-g_UA2 = ExpXsec2TGraph(xsecs_UA2, color = 1, markerstyle = 22)
+g_UA2 = ExpXsec2TGraph(xsecs_UA2, color = 1, markerstyle = 22, name = "g_UA2", title = f"UA2, {xsecs_UA2.GetLegend()}")
 # shift the x value so that they do not overlap
 xsecs_UA1.sqrts = xsecs_UA1.sqrts - 0.035
-g_UA1 = ExpXsec2TGraph(xsecs_UA1, color = 1, markerstyle = 32)
+g_UA1 = ExpXsec2TGraph(xsecs_UA1, color = 1, markerstyle = 32, name = "g_UA1", title = f"UA1, {xsecs_UA1.GetLegend()}")
 # for the label, change it back
 xsecs_UA1.sqrts = xsecs_UA1.sqrts + 0.035
 
@@ -187,7 +195,7 @@ DrawHistos(toDraws, legends, 0.3, 48, "#sqrt{s} [TeV]", 20, 3e5, "#sigma x B [pb
 
 
 # CMS only
-toDraws = toDraws[2:-4]
+toDraws_CMSOnly = toDraws[2:-4]
 drawoptions = drawoptions[2:-4]
 legends = legends[2:-4]
 legends = [legend.replace("CMS, ", "") for legend in legends]
@@ -225,4 +233,11 @@ Theory2.SetTextColor(4)
 Theory2.SetFillColor(0)
 Theory2.SetBorderSize(0)
 
-DrawHistos(toDraws, legends, 1.5, 29, "#sqrt{s} [TeV]", 1.1e2, 3e5, "#sigma x B [pb]", "xsec_CMS_Only_PAS", dologx=True, W_ref = 800, H_ref = 600, noLumi=True, additionalToDraw = [Theory2] + txts2, drawoptions = drawoptions, legendoptions = legendoptions, legendPos = [0.18, 0.64, 0.5, 0.91], legendTextSize = 0.028, doPAS = doPAS, nolabel = True, noSqrtS=True)
+DrawHistos(toDraws_CMSOnly, legends, 1.5, 29, "#sqrt{s} [TeV]", 1.1e2, 3e5, "#sigma x B [pb]", "xsec_CMS_Only_PAS", dologx=True, W_ref = 800, H_ref = 600, noLumi=True, additionalToDraw = [Theory2] + txts2, drawoptions = drawoptions, legendoptions = legendoptions, legendPos = [0.18, 0.64, 0.5, 0.91], legendTextSize = 0.028, doPAS = doPAS, nolabel = True, noSqrtS=True)
+
+
+# save the graphs for HEPData
+fout = ROOT.TFile("xsecs.root", "RECREATE")
+for g in toDraws:
+    g.Write()
+fout.Close()
